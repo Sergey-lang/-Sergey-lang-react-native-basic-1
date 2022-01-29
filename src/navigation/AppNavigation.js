c31cbc85180cb9f0310c039a7fedb8112b8156d4
Sergey <sergei.kuharyonok@yandex.by>
2022-01-29 19:51:52 +0300

    1.2 Navigation styling

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -2,16 +2,29 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { MainScreen } from '../screens/MainScreen';
 import { PostScreen } from '../screens/PostScreen';
+import { Platform } from 'react-native';
+import { THEME } from '../theme';
 
 const Stack = createNativeStackNavigator();
-
+const isIOS = Platform.OS !== 'android';
 export const AppNavigation = () => {
+
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator initialRouteName="Home" screenOptions={options}>
         <Stack.Screen name="Home" component={MainScreen} />
         <Stack.Screen name="Post" component={PostScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
-}
+};
+
+const options = {
+  headerStyle: {
+    backgroundColor: isIOS ? '#fff' : THEME.MAIN_COLOR,
+  },
+  headerTintColor: isIOS ? THEME.MAIN_COLOR : '#fff',
+  headerTitleStyle: {
+    fontWeight: 'bold',
+  },
+};
--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Button } from 'react-native';
 
-export const MainScreen = () => {
+export const MainScreen = ({navigation}) => {
+  const goTo = () => {
+    navigation.navigate('Post')
+  }
   return (
     <View style={styles.center}>
       <Text>Main screen</Text>
+      <Button title={'Go'} onPress={goTo}/>
     </View>
   );
 };
