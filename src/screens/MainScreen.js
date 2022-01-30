46440c34f39b659bac80adeee60a5c172d17e0c9
Sergey <sergei.kuharyonok@yandex.by>
2022-01-30 14:13:39 +0300

    1.3 Background image

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,23 +1,22 @@
 import React from 'react';
-import { StyleSheet, View, Text, Button } from 'react-native';
+import { StyleSheet, View, Text, Button, FlatList } from 'react-native';
+import { DATA } from '../data';
+import { Post } from '../components/Post';
 
-export const MainScreen = ({navigation}) => {
+export const MainScreen = ({ navigation }) => {
   const goTo = () => {
-    navigation.navigate('Post')
-  }
+    navigation.navigate('Post');
+  };
   return (
-    <View style={styles.center}>
-      <Text>Main screen</Text>
-      <Button title={'Go'} onPress={goTo}/>
+    <View style={styles.wrap}>
+      <FlatList data={DATA} keyExtractor={post => post.id.toString()} renderItem={({ item }) => <Post post={item} />} />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  center: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+  wrap: {
+    padding: 10,
   },
 });
 
