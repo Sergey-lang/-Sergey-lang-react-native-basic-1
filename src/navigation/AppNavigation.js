03a3ceb0c906d86c9bcf747d645f41086eea7f82
Sergey <sergei.kuharyonok@yandex.by>
2022-02-02 01:03:51 +0300

    1.5 Navigation (draft)

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -4,23 +4,64 @@ import { MainScreen } from '../screens/MainScreen';
 import { PostScreen } from '../screens/PostScreen';
 import { Platform } from 'react-native';
 import { THEME } from '../theme';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { BookedScreen } from '../screens/BookedScreen';
+import { Ionicons } from '@expo/vector-icons';
+import React from 'react';
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 const isIOS = Platform.OS !== 'android';
 
-export const AppNavigation = () => {
+function PostNavigator() {
+  return (
+    <Stack.Navigator screenOptions={{
+      headerStyle: {
+        backgroundColor: isIOS ? '#fff' : THEME.MAIN_COLOR,
+        headerTintColor: isIOS ? THEME.MAIN_COLOR : '#fff',
+      }
+    }}>
+      <Stack.Screen name="Main" component={MainScreen} />
+      <Stack.Screen name="Post" component={PostScreen} />
+    </Stack.Navigator>
+  );
+}
 
+function BookedNavigator() {
+  return (
+    <Stack.Navigator screenOptions={{
+      headerStyle: {
+        backgroundColor: isIOS ? '#fff' : THEME.MAIN_COLOR,
+        headerTintColor: isIOS ? THEME.MAIN_COLOR : '#fff'
+      }
+    }}>
+      <Stack.Screen name="Post" component={PostScreen} />
+      <Stack.Screen name="Booked" component={BookedScreen} />
+    </Stack.Navigator>
+  );
+}
+
+export const AppNavigation = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home" screenOptions={mainOptions}>
-        <Stack.Screen name="Home" component={MainScreen} />
-        <Stack.Screen name="Post" component={PostScreen} />
-      </Stack.Navigator>
+      <Tab.Navigator initialRouteName="Home" screenOptions={mainOptions}>
+        <Tab.Screen name="Post" component={PostNavigator} options={{
+          tabBarIcon: (info) => (
+            <Ionicons name="ios-albums" size={25} color={info.color} />
+          )
+        }} />
+        <Tab.Screen name="Booked" component={BookedNavigator} options={{
+          tabBarIcon: (info) => (
+            <Ionicons name="ios-star" size={25} color={info.color} />
+          )
+        }} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 };
 
 const mainOptions = {
+  headerShown: false,
   headerStyle: {
     backgroundColor: isIOS ? '#fff' : THEME.MAIN_COLOR,
   },
