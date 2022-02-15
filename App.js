5e791a10418ba9ae602fcd5abcf84b2cc180dfa6
Sergey <sergei.kuharyonok@yandex.by>
2022-02-15 23:09:21 +0300

    3. Connect redux

--- App.js
+++ App.js
@@ -2,17 +2,22 @@ import { useState } from 'react';
 import AppLoading from 'expo-app-loading';
 import { bootstrap } from './src/bootstrap';
 import { AppNavigation } from './src/navigation/AppNavigation';
-
+import { Provider } from 'react-redux';
+import store from './src/store';
 
 export default function App() {
-  const [isReady, setIsReady] =useState(false);
+  const [isReady, setIsReady] = useState(false);
 
-  if(!isReady) {
+  if (!isReady) {
     return <AppLoading startAsync={bootstrap}
                        onError={(err) => console.log(err)}
                        onFinish={() => setIsReady(true)}
-    />
+    />;
   }
 
-  return <AppNavigation />
+  return (
+    <Provider store={store}>
+      <AppNavigation />
+    </Provider>
+  );
 }
