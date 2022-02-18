012aa356a30e6d71fe0b9c890a10c7aee0c5c678
Sergey <sergei.kuharyonok@yandex.by>
2022-02-18 23:58:01 +0300

    4.1 Create DB

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,8 +1,18 @@
 import React from 'react';
-import { FlatList, StyleSheet, View } from 'react-native';
+import { FlatList, StyleSheet, View, Text } from 'react-native';
 import { Post } from './Post';
 
 export const PostList = ({ data, onOpen }) => {
+
+  if (!data.length) {
+    return (
+      <View style={styles.wrap}>
+        <Text style={styles.noItems}>
+          No posts...
+        </Text>
+      </View>
+    );
+  }
 
   return (
     <View style={styles.wrap}>
@@ -17,6 +27,12 @@ const styles = StyleSheet.create({
   wrap: {
     padding: 10,
   },
+  noItems: {
+    fontFamily: 'open-sans-regular',
+    textAlign: 'center',
+    margin: 10,
+    fontSize: 18,
+  }
 });
 
 
